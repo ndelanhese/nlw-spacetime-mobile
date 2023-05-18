18c9fe41564d3ada7ec186f1ac01191b54aac444
ndelanhese <nnathanh3@gmail.com>
2023-05-18 12:14:41 -0300

    feat(app): added login oauth with github and secure storage token

--- App.tsx
+++ App.tsx
@@ -6,13 +6,23 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import React from 'react'
+import React, { useEffect } from 'react'
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { api } from './src/lib/api'
+import * as SecureStore from 'expo-secure-store'
 
 const StyledStrips = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/2128bdb10a5b89c8a1be',
+}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +30,31 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '2128bdb10a5b89c8a1be',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlw-spacetime',
+      }),
+    },
+    discovery,
+  )
+  console.log(request) // only to commit
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      api
+        .post('/register', { code })
+        .then((response) => {
+          const { token } = response.data
+          SecureStore.setItemAsync('token', token)
+        })
+        .catch((error) => console.error(error))
+    }
+  }, [response])
 
   if (!fontsLoaded) {
     return null
@@ -47,7 +82,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
-          onPress={() => console.log('pressed')}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             cadastrar lembrança
