1cf34b401b52ebcbbaf5ffd3ffeef6ac6d1c45a7
ndelanhese <nnathanh3@gmail.com>
2023-05-22 20:30:20 -0300

    feat(memoriy): create memories

--- app/new.tsx
+++ app/new.tsx
@@ -1,4 +1,5 @@
 import {
+  Image,
   ScrollView,
   Switch,
   Text,
@@ -7,14 +8,60 @@ import {
   View,
 } from 'react-native'
 import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import React, { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
 
 export default function New() {
+  const router = useRouter()
   const { bottom, top } = useSafeAreaInsets()
+  const [preview, setPreview] = useState(null)
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function handleCreateNewMemory() {
+    const token = SecureStore.getItemAsync('token')
+    let coverUrl = ''
+    if (preview) {
+      const uploadFormData = new FormData()
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+      coverUrl = uploadResponse.data.fileURL
+    }
+    await api.post(
+      '/memories',
+      { content, coverUrl, isPublic },
+      { headers: { Authorization: `Bearer ${token}` } },
+    )
+    router.push('/memories')
+  }
+  const toggleSwitch = () => {
+    setIsPublic(!isPublic)
+  }
   return (
     <ScrollView
       className="flex-1 px-8"
@@ -35,33 +82,54 @@ export default function New() {
             onValueChange={setIsPublic}
             trackColor={{ false: '#767577', true: '#372560' }}
             thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+            id="public-switch"
           />
-          <Text className="font-body text-base text-gray-200">
+          <Text
+            onPress={toggleSwitch}
+            className="font-body text-base text-gray-200"
+          >
             Tornar memória pública
           </Text>
         </View>
         <TouchableOpacity
           activeOpacity={0.7}
           className="border-dash h-32 items-center justify-center rounded-lg border border-gray-500 bg-black/20"
+          onPress={openImagePicker}
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+              alt="coverage Image"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
         <TextInput
           multiline
+          textAlignVertical="top"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
           placeholderTextColor="#56565a"
           className=" p-0 font-body text-lg text-gray-50"
+          value={content}
+          onChangeText={setContent}
         />
         <TouchableOpacity
           activeOpacity={0.7}
           className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
-          <Text className="font-alt text-sm uppercase text-black">salvar</Text>
+          <Text
+            className="font-alt text-sm uppercase text-black"
+            onPress={handleCreateNewMemory}
+          >
+            salvar
+          </Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
